967e38134795badd8f3e1f82a1cad1a5a2c30ec5
Christine Step <1511921+csteppp@users.noreply.github.com>
2021-05-18 08:34:39 -0400

    A simple working baby network.
    http based, haproxy tier 7 in front, two nginx in back.

--- run-nginx1.bat
+++
@@ -1,3 +0,0 @@
-docker stop mynginx1
-docker rm mynginx1 
-docker run --name mynginx1 -p 91:80 -d nginx1_image1
--- run-nginx2.bat
+++
@@ -1,3 +0,0 @@
-docker stop mynginx2 
-docker rm mynginx2 
-docker run --name mynginx2 -p 90:80 -d nginx2_image1
